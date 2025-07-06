28f3b6b646ab0cc3ba802e6c2037d08a9bd21783
Prateek-BITS <2021wb86573@wilp.bits-pilani.ac.in>
2025-07-06 18:13:11 +0530

    Updated message for CI

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -2,6 +2,6 @@ package com.bits;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Lab!");
+        System.out.println("Updated message for Jenkins CI!");
     }
 }
